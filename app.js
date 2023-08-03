a5acce042fe9f9fab66a1f4dc0e9a7a3c958d66a
JonathanTylerWeber <jonathantweber@gmail.com>
2023-08-03 19:44:22 +0800

    done

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener('mousemove', function(e){
+    const g = Math.round(e.pageX * 255 / window.innerWidth);
+    const b = Math.round(e.pageY * 255 / window.innerHeight);
+    const color = `rgb(0,${g},${b})`;
+    document.body.style.backgroundColor = color;
+    console.log(color);
+})
